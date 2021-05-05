bc711fc29f88637998c9a4def94ef9999b5b1f6f
Mahiudidin Al Kamal <mhak@mms-t-systems.de>
2021-05-05 07:16:05 +0200

    updated ERD, schema script and added countries data script

--- Ecommerce Solution DB Final.sql
+++ Ecommerce Solution DB Final.sql
@@ -1,5 +1,9 @@
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+
+
 CREATE TABLE "customers" (
-  "customer_id" int PRIMARY KEY,
+  "customer_id" BIGSERIAL PRIMARY KEY,
   "first_name" varchar,
   "last_name" varchar,
   "house_no" varchar,
@@ -10,11 +14,11 @@ CREATE TABLE "customers" (
   "country" varchar,
   "mobile_no" varchar,
   "email" varchar,
-  "date_of_birth" datetime
+  "date_of_birth" timestamp
 );
 
 CREATE TABLE "temp_orders" (
-  "temp_order_id" int PRIMARY KEY,
+  "temp_order_id" BIGSERIAL PRIMARY KEY,
   "total_price" float,
   "discount_amount" float,
   "grand_total_price" float,
@@ -30,7 +34,7 @@ CREATE TABLE "temp_orders" (
 );
 
 CREATE TABLE "temp_order_items" (
-  "temp_order_item_id" int PRIMARY KEY,
+  "temp_order_item_id" BIGSERIAL PRIMARY KEY,
   "fk_temp_order_id" int,
   "fk_product_id" int,
   "order_item_name" varchar,
@@ -40,7 +44,7 @@ CREATE TABLE "temp_order_items" (
 );
 
 CREATE TABLE "orders" (
-  "order_id" int PRIMARY KEY,
+  "order_id" BIGSERIAL PRIMARY KEY,
   "total_price" float,
   "discount_amount" float,
   "grand_total_price" float,
@@ -56,7 +60,7 @@ CREATE TABLE "orders" (
 );
 
 CREATE TABLE "order_items" (
-  "order_item_id" int PRIMARY KEY,
+  "order_item_id" BIGSERIAL PRIMARY KEY,
   "fk_order_id" int,
   "fk_product_id" int,
   "order_item_name" varchar,
@@ -66,12 +70,12 @@ CREATE TABLE "order_items" (
 );
 
 CREATE TABLE "payment_types" (
-  "payment_type_id" int PRIMARY KEY,
+  "payment_type_id" BIGSERIAL PRIMARY KEY,
   "payment_type_name" varchar
 );
 
 CREATE TABLE "transactions" (
-  "transaction_id" int PRIMARY KEY,
+  "transaction_id" BIGSERIAL PRIMARY KEY,
   "transcation_amount" float,
   "fk_currency_id" int,
   "fk_order_id" int,
@@ -80,50 +84,50 @@ CREATE TABLE "transactions" (
 );
 
 CREATE TABLE "shopping_temp_carts" (
-  "shopping_temp_cart_id" int PRIMARY KEY,
+  "shopping_temp_cart_id" BIGSERIAL PRIMARY KEY,
   "fk_product_id" int,
   "fk_customer_id" int,
   "item_quantity" int
 );
 
 CREATE TABLE "status_catalogues" (
-  "status_id" int PRIMARY KEY,
+  "status_id" BIGSERIAL PRIMARY KEY,
   "status_name" varchar,
   "status_desc" varchar
 );
 
 CREATE TABLE "deliveries" (
-  "delivery_id" int PRIMARY KEY,
+  "delivery_id" BIGSERIAL PRIMARY KEY,
   "fk_order_id" int,
   "person_name" int,
   "contact_no" varchar,
-  "alloted_time" datetime,
-  "recieved_time" datetime
+  "alloted_time" timestamp,
+  "recieved_time" timestamp
 );
 
 CREATE TABLE "shippings" (
-  "shipping_id" int PRIMARY KEY
+  "shipping_id" BIGSERIAL PRIMARY KEY
 );
 
 CREATE TABLE "cancellations" (
-  "cancellation_id" int PRIMARY KEY
+  "cancellation_id" BIGSERIAL PRIMARY KEY
 );
 
 CREATE TABLE "brands" (
-  "brand_id" int PRIMARY KEY,
+  "brand_id" BIGSERIAL PRIMARY KEY,
   "brand_name" varchar,
   "brand_description" varchar
 );
 
 CREATE TABLE "categories" (
-  "category_id" int PRIMARY KEY,
+  "category_id" BIGSERIAL PRIMARY KEY,
   "category_name" varchar,
   "category_description" varchar,
   "parent_category" int
 );
 
 CREATE TABLE "products" (
-  "product_id" int PRIMARY KEY,
+  "product_id" BIGSERIAL PRIMARY KEY,
   "product_name" text,
   "product_description" text,
   "fk_brand_id" int,
@@ -136,39 +140,39 @@ CREATE TABLE "products" (
 );
 
 CREATE TABLE "product_discounts" (
-  "product_discount_id" int PRIMARY KEY,
+  "product_discount_id" BIGSERIAL PRIMARY KEY,
   "fk_product_id" int,
   "fk_discount_type_id" int,
   "discount_amount" float,
   "fk_currency_id" int,
-  "discount_start_date" datetime,
-  "discount_end_date" datetime
+  "discount_start_date" timestamp,
+  "discount_end_date" timestamp
 );
 
 CREATE TABLE "discount_types" (
-  "discount_type_id" int PRIMARY KEY,
+  "discount_type_id" BIGSERIAL PRIMARY KEY,
   "discount_type_name" varchar
 );
 
 CREATE TABLE "product_variation_types" (
-  "product_var_type_id" int PRIMARY KEY,
+  "product_var_type_id" BIGSERIAL PRIMARY KEY,
   "var_type_name" varchar,
   "var_type_description" varchar
 );
 
 CREATE TABLE "product_variation_type_options" (
-  "product_var_type_option_id" int PRIMARY KEY
+  "product_var_type_option_id" BIGSERIAL PRIMARY KEY
 );
 
 CREATE TABLE "product_images" (
-  "product_images_id" int PRIMARY KEY,
+  "product_images_id" BIGSERIAL PRIMARY KEY,
   "fk_product_id" int,
   "product_image_name" varchar,
   "product_image_path" varchar
 );
 
 CREATE TABLE "shops" (
-  "shop_id" int PRIMARY KEY,
+  "shop_id" BIGSERIAL PRIMARY KEY,
   "shop_name" varchar,
   "shop_description" varchar,
   "shop_address" varchar,
@@ -176,45 +180,54 @@ CREATE TABLE "shops" (
 );
 
 CREATE TABLE "locations" (
-  "location_id" int PRIMARY KEY,
+  "location_id" BIGSERIAL PRIMARY KEY,
   "location_name" varchar,
   "fk_country_id" int
 );
 
-CREATE TABLE "countries" (
-  "country_id" int PRIMARY KEY,
-  "country_name" varchar
+
+CREATE SEQUENCE countries_country_id_seq;
+
+CREATE TABLE IF NOT EXISTS countries (
+  country_id int NOT NULL DEFAULT NEXTVAL ('countries_country_id_seq'),
+  iso char(2) NOT NULL,
+  country_name varchar(80) NOT NULL,
+  nicename varchar(80) NOT NULL,
+  iso3 char(3) DEFAULT NULL,
+  numcode smallint DEFAULT NULL,
+  phonecode int NOT NULL,
+  PRIMARY KEY (country_id)
 );
 
 CREATE TABLE "currencies" (
-  "currency_id" int PRIMARY KEY,
+  "currency_id" BIGSERIAL PRIMARY KEY,
   "currency_name" varchar,
   "currency_sign" varchar
 );
 
 CREATE TABLE "delivery_schedules" (
-  "delivery_schedule_id" int PRIMARY KEY,
+  "delivery_schedule_id" BIGSERIAL PRIMARY KEY,
   "delivery_schedule_slab" int,
-  "delivery_schedule_start" datetime,
-  "delivery_schedule_end" datetime,
+  "delivery_schedule_start" timestamp,
+  "delivery_schedule_end" timestamp,
   "fk_day_type_id" int
 );
 
 CREATE TABLE "day_types" (
-  "day_type_id" int PRIMARY KEY,
+  "day_type_id" BIGSERIAL PRIMARY KEY,
   "day_type_name" int
 );
 
 CREATE TABLE "coupons" (
-  "coupon_id" int PRIMARY KEY,
+  "coupon_id" BIGSERIAL PRIMARY KEY,
   "coupon_amount" float,
   "fk_currency_id" int,
-  "coupon_start_date" datetime,
-  "coupon_end_date" datetime
+  "coupon_start_date" timestamp,
+  "coupon_end_date" timestamp
 );
 
 CREATE TABLE "delivery_costs" (
-  "delivery_cost_id" int PRIMARY KEY,
+  "delivery_cost_id" BIGSERIAL PRIMARY KEY,
   "delivery_slab_start_range" int,
   "delivery_slab_end_range" int,
   "delivery_cost" float,
@@ -222,7 +235,7 @@ CREATE TABLE "delivery_costs" (
 );
 
 CREATE TABLE "users" (
-  "user_id" int PRIMARY KEY,
+  "user_id" BIGSERIAL PRIMARY KEY,
   "fk_customer_id" int,
   "user_name" varchar,
   "user_email" varchar,
@@ -231,46 +244,46 @@ CREATE TABLE "users" (
 );
 
 CREATE TABLE "admin_users" (
-  "admin_user_id" int PRIMARY KEY,
+  "admin_user_id" BIGSERIAL PRIMARY KEY,
   "user_name" varchar,
   "password" varchar,
   "salt" varchar
 );
 
 CREATE TABLE "permissions" (
-  "permission_id" int PRIMARY KEY,
+  "permission_id" BIGSERIAL PRIMARY KEY,
   "permission_name" text,
   "permission_description" text
 );
 
 CREATE TABLE "roles" (
-  "role_id" int PRIMARY KEY,
+  "role_id" BIGSERIAL PRIMARY KEY,
   "role_name" text,
   "role_description" text,
   "fk_permission_id" int
 );
 
 CREATE TABLE "user_role_links" (
-  "admin_user_id" int,
-  "role_id" int,
+  "admin_user_id" BIGSERIAL,
+  "role_id" BIGSERIAL,
   PRIMARY KEY ("admin_user_id", "role_id")
 );
 
 CREATE TABLE "menus" (
-  "menu_id" int PRIMARY KEY,
+  "menu_id" BIGSERIAL PRIMARY KEY,
   "menu_name" text,
   "menu_description" text
 );
 
 CREATE TABLE "modules" (
-  "module_id" int PRIMARY KEY,
+  "module_id" BIGSERIAL PRIMARY KEY,
   "module_name" text,
   "module_description" text
 );
 
 CREATE TABLE "menu_module_link" (
-  "menu_id" int,
-  "module_id" int,
+  "menu_id" BIGSERIAL,
+  "module_id" BIGSERIAL,
   PRIMARY KEY ("menu_id", "module_id")
 );
 
@@ -362,3 +375,10 @@ ALTER TABLE "users" ADD FOREIGN KEY ("fk_customer_id") REFERENCES "customers" ("
 
 ALTER TABLE "roles" ADD FOREIGN KEY ("fk_permission_id") REFERENCES "permissions" ("permission_id");
 
+ALTER TABLE "menu_module_link" ADD FOREIGN KEY ("menu_id") REFERENCES "menus" ("menu_id");
+
+ALTER TABLE "menu_module_link" ADD FOREIGN KEY ("module_id") REFERENCES "modules" ("module_id");
+
+ALTER TABLE "user_role_links" ADD FOREIGN KEY ("admin_user_id") REFERENCES "admin_users" ("admin_user_id");
+
+ALTER TABLE "user_role_links" ADD FOREIGN KEY ("role_id") REFERENCES "roles" ("role_id");
